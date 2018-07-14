4d200c93ee9862e21fd0dc23eb64117a04f5ffc1
Denise Erazmus <denise.erazmus@gmail.com>
2018-07-14 15:20:52 -0500

    inital file set up and css background

---
+++ react-list/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+           <h1 className="App-title">React List</h1>
+          <h2> By Denise Appler Erazmus </h2>
+        </header>
+        <p className="quotenpmn">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
