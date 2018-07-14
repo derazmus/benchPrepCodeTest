6e0fd247fc6b84b23f17f6612a308441538dd181
Denise Erazmus <denise.erazmus@gmail.com>
2018-07-14 17:15:53 -0500

    grabbing JSON data

--- react-list/src/App.js
+++ react-list/src/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
 class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state={
+    data: null,
+      quotesList: [],
+      currentPage: 1,
+      quotesPerPage: 15,
+      theme: '',
+      search: '',
+  }
+}
+
+getNewQuote(){
+  axios.get('https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/b191cf3b6ea9cdcca8b363516ff969261398061f/quotes.json')
+  .then((response)=>this.setState({quote:response.data}))
+}
+
+componentDidMount(){
+  this.getNewQuote();
+}
+
   render() {
     return (
       <div className="App">
@@ -9,10 +33,9 @@ class App extends Component {
            <h1 className="App-title">React List</h1>
           <h2> By Denise Appler Erazmus </h2>
         </header>
-        <p className="quotenpmn">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        
+    </div>
+   
     );
   }
 }
