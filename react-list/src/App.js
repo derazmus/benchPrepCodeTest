3db05d4d81dafd90145c7fa1fce256a2108c9096
Denise Erazmus <denise.erazmus@gmail.com>
2018-07-15 01:55:35 -0500

    displaying json data, formatted table

--- react-list/src/App.js
+++ react-list/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import quoteList from './quoteList.json';
 
 
 class App extends Component {
@@ -8,18 +9,18 @@ class App extends Component {
 constructor(props){
   super(props);
   this.state={
-    data: null,
-      quotesList: [],
-      currentPage: 1,
-      quotesPerPage: 15,
-      theme: '',
-      search: '',
-  }
+     quoteList:[]
+  };
 }
 
 getNewQuote(){
   axios.get('https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/b191cf3b6ea9cdcca8b363516ff969261398061f/quotes.json')
-  .then((response)=>this.setState({quote:response.data}))
+  .then(({data})=> {
+    this.setState({
+      quoteList:data
+    });
+})
+  .catch((err) => {})
 }
 
 componentDidMount(){
@@ -27,16 +28,38 @@ componentDidMount(){
 }
 
   render() {
-    return (
+    return ([
+ 
       <div className="App">
         <header className="App-header">
            <h1 className="App-title">React List</h1>
-          <h2> By Denise Appler Erazmus </h2>
-        </header>
-        
-    </div>
-   
-    );
+           <h1 className="App-title"> Denise Appler Erazmus </h1>
+        </header>  
+        <table>
+          <thead>
+            <tr>
+              <th>Source</th>
+              <th>Context</th>
+              <th>Quote</th>
+              <th>Theme</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            quoteList.map(row =>(
+              <tr>
+                <td>{row.source}</td>
+                <td>{row.context}</td>
+                <td>{row.quote}</td>
+                <td>{row.theme}</td>
+              </tr>
+              ))
+          }
+          </tbody>
+        </table>
+      </div>
+      
+   ]);
   }
 }
 
